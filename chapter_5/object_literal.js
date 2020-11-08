8efd74d16d477c492b8810494b12ccfc9008a157
Ely Bourland <JEBourland@yahoo.com>
2020-11-07 21:35:21 -0800

    object literals

--- chapter_5/object_literal.js
+++ chapter_5/object_literal.js
@@ -1,2 +1,25 @@
 // object literals
 
+let user = {
+    name: 'ely',
+    age: 38,
+    email: 'ely@thenetninja.co.uk',
+    location: 'berlin',
+    blogs: ['why mac & cheese rules', '10 things to make with marmite']
+};
+
+console.log(user);
+console.log(user.name);
+
+// user.age = 39;
+console.log(user.age);
+
+const key = 'location';
+
+console.log(user['location']);
+
+user['name'] = 'chun-li';
+
+console.log(user['name']);
+
+console.log(typeof user);
