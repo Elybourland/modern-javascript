aa8eb21763132e11a39cea88dfb91def7ffed04d
Ely Bourland <JEBourland@yahoo.com>
2020-11-08 14:05:47 -0800

    this

---
+++ chapter_5/this_keyword.js
@@ -0,0 +1,18 @@
+// This keyword
+
+let user = {
+    name: 'ely',
+    age: 38,
+    email: 'ely@thenetninja.co.uk',
+    location: 'berlin',
+    blogs: ['why mac & cheese rules', '10 things to make with marmite'],
+    login: function() {
+        console.log('the user logged in');
+    },
+    logout: function() {
+        console.log('the user logged out');
+    },
+    logBlogs: function() {
+
+    }
+};
