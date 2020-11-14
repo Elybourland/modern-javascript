6aa4ec60e64f63ae11c8ba6c92d7506951a1dc2a
Ely Bourland <JEBourland@yahoo.com>
2020-11-14 14:26:34 -0800

    this keyword example

--- chapter_5/this_keyword.js
+++ chapter_5/this_keyword.js
@@ -6,13 +6,20 @@ let user = {
     email: 'ely@thenetninja.co.uk',
     location: 'berlin',
     blogs: ['why mac & cheese rules', '10 things to make with marmite'],
-    login: function() {
+    login() {
         console.log('the user logged in');
     },
-    logout: function() {
+    logout() {
         console.log('the user logged out');
     },
-    logBlogs: function() {
-
+    logBlogs() {
+        // console.log(this.blogs);
+        console.log('this user has written the following blogs:');
+        this.blogs.forEach(blog => {
+            console.log(blog);
+        });
     }
 };
+
+user.logBlogs();
+console.log(this);
