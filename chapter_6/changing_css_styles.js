86729abd4a0df3c822b5a845926544cf79f32c29
Ely Bourland <JEBourland@yahoo.com>
2020-11-15 07:41:10 -0800

    changing css styles

---
+++ chapter_6/changing_css_styles.js
@@ -0,0 +1,14 @@
+const title = document.querySelector('h1');
+
+console.log(title.style);
+console.log(title.style.color);
+
+title.style.margin = '50px';
+
+title.style.color = 'crimson';
+
+title.style.fontSize = '60px';
+
+// Removing style use empty string
+
+title.style.margin = '';
