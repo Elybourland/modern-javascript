4d18912d2a49d0b58484767756bddc1cf373b998
Ely Bourland <JEBourland@yahoo.com>
2020-11-15 08:01:28 -0800

    adding and removing classes

---
+++ chapter_6/adding_removing_classes.js
@@ -0,0 +1,29 @@
+// Adding & Removing classes
+
+// const content = document.querySelector('p');
+
+// console.log(content.classList);
+
+// content.classList.add('error');
+// content.classList.remove('error');
+
+// content.classList.add('success');
+
+const paras = document.querySelectorAll('p');
+
+paras.forEach(p => {
+   if(p.textContent.includes('error')){
+       p.classList.add('error');
+   } 
+   if(p.textContent.includes('success')){
+       p.classList.add('success');
+   }
+});
+
+// Toggle a class
+
+const title = document.querySelector('.title');
+
+title.classList.toggle('test');
+title.classList.toggle('test');
+
